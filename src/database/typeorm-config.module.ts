8f62b0d26910e6dd0b2eb636de993580f87b00b2
Mahmudul Murad <murad@simecsystem.com>
2023-09-04 18:46:54 +0600

    dockerizing nestjs backend

--- src/database/typeorm-config.module.ts
+++ src/database/typeorm-config.module.ts
@@ -9,11 +9,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: configService.get('DB_HOST'),
+        host: 'db',
+        port: 3306,
+        username: 'root',
+        password: 'New@Password@123',
+        database: 'tldatabase',
+        /* host: configService.get('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
+        database: configService.get('DB_DATABASE'), */
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
         logging: true,
